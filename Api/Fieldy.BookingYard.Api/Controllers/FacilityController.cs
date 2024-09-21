214279bdc4fe5e5d03c4add553d33a7266f26cf9
huynhduc <lengochuynhduc7@gmail.com>
2024-09-21 18:55:21 +0700

    Feature(Court): add api CRU

--- Api/Fieldy.BookingYard.Api/Controllers/FacilityController.cs
+++ Api/Fieldy.BookingYard.Api/Controllers/FacilityController.cs
@@ -1,5 +1,9 @@
 using System.Net.Mime;
 using Fieldy.BookingYard.Application.Features.Facility.Commands.CreateFacility;
+using Fieldy.BookingYard.Application.Features.Facility.Queries;
+using Fieldy.BookingYard.Application.Features.Facility.Queries.GetAllFacilityAdmin;
+using Fieldy.BookingYard.Application.Models.Paging;
+using Fieldy.BookingYard.Application.Models.Query;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +28,6 @@ namespace Fieldy.BookingYard.Api.Controllers
         [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateFacility(
            [FromForm] CreateFacilityCommand command,
@@ -32,6 +35,21 @@ namespace Fieldy.BookingYard.Api.Controllers
         {
             var result = await _mediator.Send(command, cancellationToken);
             return Created("", result);
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(PagingResult<FacilityAdminDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetFacilityAdmin(
+           [FromQuery] RequestParams request,
+           CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new GetAllFacilityAdminQuery(request), cancellationToken);
+            return Ok(result);
         }
     }
 }
