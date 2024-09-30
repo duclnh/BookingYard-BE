3a956eeb8b8290cfc2e5b3abdbfff34bba7a448d
huynhduc <lengochuynhduc7@gmail.com>
2024-09-30 11:38:27 +0700

    FixError(register): fix register account

--- Core/Fieldy.BookingYard.Application/Features/Feedback/Queries/GetAllFeedback/GetAllFeedbackHandler.cs
+++ Core/Fieldy.BookingYard.Application/Features/Feedback/Queries/GetAllFeedback/GetAllFeedbackHandler.cs
@@ -1,6 +1,9 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using Fieldy.BookingYard.Application.Abstractions;
 using Fieldy.BookingYard.Application.Models.Paging;
 using Fieldy.BookingYard.Domain.Abstractions.Repositories;
+using Fieldy.BookingYard.Domain.Enums;
 using MediatR;
 
 namespace Fieldy.BookingYard.Application.Features.Feedback.Queries.GetAllFeedback
@@ -9,32 +12,47 @@ namespace Fieldy.BookingYard.Application.Features.Feedback.Queries.GetAllFeedbac
 	{
 		private readonly IMapper _mapper;
 		private readonly IFeedbackRepository _FeedbackRepository;
+		private readonly IJWTService _jWTService;
+		private readonly IUserRepository _userRepository;
 
-		public GetAllFeedbackHandler(IMapper mapper, IFeedbackRepository FeedbackRepository)
+		public GetAllFeedbackHandler(IMapper mapper, IFeedbackRepository feedbackRepository, IJWTService jWTService, IUserRepository userRepository)
 		{
 			_mapper = mapper;
-			_FeedbackRepository = FeedbackRepository;
+			_FeedbackRepository = feedbackRepository;
+			_jWTService = jWTService;
+			_userRepository = userRepository;
 		}
 
 		public async Task<PagingResult<FeedbackDto>> Handle(GetAllFeedbackQuery request, CancellationToken cancellationToken)
 		{
+			var user = await _userRepository.Find(x => x.Id == _jWTService.UserID, cancellationToken);
+
+			Expression<Func<Domain.Entities.FeedBack, bool>>? expression = null;
+			
+			if (user != null && user.Role == Role.CourtOwner)
+			{
+				expression =  x => x.FacilityID == request.facilityID;
+			}
+
 			var listFeedback = await _FeedbackRepository.FindAllPaging(
 				currentPage: request.requestParams.CurrentPage,
-				pageSize: Math.Min(request.requestParams.PageSize, 10),
-				expression: x => x.FacilityID == request.FaciliyId,
+				pageSize: request.requestParams.PageSize,
+				expression: expression,
 				orderBy: x => x.OrderByDescending(x => x.CreatedAt),
 				includes: x => x.Images,
-				cancellationToken: cancellationToken);
+				cancellationToken: cancellationToken
+			);
 
 			return PagingResult<FeedbackDto>.Create(
-			   totalCount: listFeedback.TotalCount,
-			   pageSize: listFeedback.PageSize,
-			   currentPage: listFeedback.CurrentPage,
-			   totalPages: listFeedback.TotalPages,
-			   hasNext: listFeedback.HasNext,
-			   hasPrevious: listFeedback.HasPrevious,
-			   results: _mapper.Map<IList<FeedbackDto>>(listFeedback.Results)
-		   );
+				totalCount: listFeedback.TotalCount,
+				pageSize: listFeedback.PageSize,
+				currentPage: listFeedback.CurrentPage,
+				totalPages: listFeedback.TotalPages,
+				hasNext: listFeedback.HasNext,
+				hasPrevious: listFeedback.HasPrevious,
+				results: _mapper.Map<IList<FeedbackDto>>(listFeedback.Results)
+			);
+
 		}
 	}
 }
