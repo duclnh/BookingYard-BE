2ddf973834a20e800fec527fe49607cd9530409e
PhonggLuu <luuphong016@gmail.com>
2024-10-01 17:50:32 +0700

    Fix(Booking): Return BookingId when create new one. Fix(Payment): Replace type price.

--- Core/Fieldy.BookingYard.Application/Features/Payment/Commands/CreatePayment/CreatePaymentCommand.cs
+++ Core/Fieldy.BookingYard.Application/Features/Payment/Commands/CreatePayment/CreatePaymentCommand.cs
@@ -5,6 +5,6 @@ namespace Fieldy.BookingYard.Application.Features.Payment.Commands.CreatePayment
 	public record CreatePaymentCommand: IRequest<string>
 	{
 		public Guid BookingId { get; set; }
-		public int RequiredAmount { get; set; }
+		public decimal RequiredAmount { get; set; }
 	}
 }
