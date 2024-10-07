71acf7649ef14ed40dbe0c7dac65a43141cf1b0d
PhonggLuu <luuphong016@gmail.com>
2024-10-07 22:50:52 +0700

    Restore payment commands

---
+++ Core/Fieldy.BookingYard.Application/Features/Payment/Commands/CreatePayment/CreatePaymentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Fieldy.BookingYard.Application.Features.Payment.Commands.CreatePayment
+{
+	public record CreatePaymentCommand: IRequest<string>
+	{
+		public Guid BookingId { get; set; }
+		public decimal RequiredAmount { get; set; }
+	}
+}
