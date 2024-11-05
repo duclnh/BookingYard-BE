62df5e61413aa8963f5ef6a7154730e1715b93b1
Lê Ngọc  Huỳnh Đức <132675206+duclnh@users.noreply.github.com>
2024-11-06 02:35:38 +0700

    Update GetAllFeedbackFacilityQueryHandler.cs

--- Core/Fieldy.BookingYard.Application/Features/Feedback/Queries/GetAllFeedbackFacility/GetAllFeedbackFacilityQueryHandler.cs
+++ Core/Fieldy.BookingYard.Application/Features/Feedback/Queries/GetAllFeedbackFacility/GetAllFeedbackFacilityQueryHandler.cs
@@ -31,7 +31,7 @@ public class GetAllFeedbackFacilityQueryHandler : IRequestHandler<GetAllFeedback
             x => x.Images
         );
 
-        var feedbackFacilitiesMapped = _mapper.Map<IList<FeedbackFacilityDetailDTO>>(feedbackFacilities.Results.Distinct().ToList());
+        var feedbackFacilitiesMapped = _mapper.Map<IList<FeedbackFacilityDetailDTO>>(feedbackFacilities.Results.DistinctBy(q => q.UserID));
 
         return PagingResult<FeedbackFacilityDetailDTO>.Create(
             totalCount: feedbackFacilities.TotalCount,
